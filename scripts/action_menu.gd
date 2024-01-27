702010690b1ff0102ced47dc95a679efe86d55e9
RothioTome <137085727+rothiotome@users.noreply.github.com>
2024-01-27 16:11:00 +0100

    basic navigation menu

--- scripts/action_menu.gd
+++ scripts/action_menu.gd
@@ -1,11 +1,49 @@
 extends HBoxContainer
 
+var actions: PackedStringArray
 
-# Called when the node enters the scene tree for the first time.
+var current_highlighted_action: String
+var default_highlighted_action: String = "LookOutWindow"
+const highlighted_alfa = 1
+const faded_alfa = 0.5
+
 func _ready():
-	pass # Replace with function body.
+	for n in get_children():
+		actions.append(n.name)
+	highlight_action(default_highlighted_action)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func highlight_action(action_name):
+	if current_highlighted_action:
+		remove_highlight_action(current_highlighted_action)
+	current_highlighted_action = action_name
+	var action_texture: TextureRect = get_texture_from_name(action_name)
+	action_texture.self_modulate.a = highlighted_alfa
+
+func remove_highlight_action(action_name):
+	if actions.has(action_name):
+		var action_texture: TextureRect = get_texture_from_name(action_name)
+		action_texture.self_modulate.a = faded_alfa
+
+func on_button_clicked(action_type):
+	match action_type:
+		"right":
+			var action_index = actions.find(current_highlighted_action)
+			if action_index == actions.size() - 1:
+				return
+			highlight_action(actions[action_index + 1])
+			
+		"left":
+			var action_index = actions.find(current_highlighted_action)
+			if action_index == 0:
+				return
+			highlight_action(actions[action_index - 1])
+		"select":
+			do_action(current_highlighted_action)
+
+func do_action(action_name):
 	pass
+	
+func get_texture_from_name(action_name):
+	if actions.has(action_name):
+		return get_child(actions.find(action_name)) as TextureRect
--- scripts/drag_window.gd
+++ scripts/drag_window.gd
@@ -3,6 +3,15 @@ extends Control
 var following: bool
 var dragging_start_position: Vector2
 
+@onready var action_menu = %ActionMenu
+
+
+
+
+func _ready():
+	DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_TRANSPARENT, true, 0)
+	DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS, true, 0)
+
 
 func _input(event):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
@@ -12,10 +21,20 @@ func _input(event):
 		if event.is_released():
 			following = false
 
+
 func _gui_input(event):
 	if following and InputEventMouseMotion:
 		DisplayServer.window_set_position(DisplayServer.window_get_position_with_decorations() as Vector2 + get_global_mouse_position() - dragging_start_position)
 
-func _ready():
-	DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_TRANSPARENT, true, 0)
-	DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS, true, 0)
+
+
+func _on_select_pressed():
+	action_menu.on_button_clicked("select")
+
+
+func _on_right_pressed():
+	action_menu.on_button_clicked("right")
+
+
+func _on_left_pressed():
+	action_menu.on_button_clicked("left")
