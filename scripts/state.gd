e5aab4190b8543baef82568d8709c9b56f5980c6
RothioTome <137085727+rothiotome@users.noreply.github.com>
2024-01-28 01:09:34 +0100

    grow tween refactor

--- scripts/state.gd
+++ scripts/state.gd
@@ -3,21 +3,22 @@ extends TextureRect
 var target_value = 0
 var current_value = 0
 
-var tween
+var grow_tween: Tween
 
 func _ready():
 	set_shader_value(current_value)
 
 func set_value(value):
 	target_value = value
-	if tween:
-		tween.stop()
-		tween.kill()
-	tween = create_tween()
-	tween.tween_method(set_shader_value, current_value, target_value, 1)
-	tween.play()
+	if grow_tween:
+		grow_tween.stop()
+		grow_tween.kill()
+	grow_tween = create_tween()
+	grow_tween.tween_method(set_shader_value, current_value, target_value, 1)
+	grow_tween.play()
 
 
 func set_shader_value(value: float):
 	material.set_shader_parameter("current_val", value);
 	current_value = value
+		
